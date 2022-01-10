affa087483c99779103ea1f9bfb5b1991540abdf
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-10 16:38:41 +0100

    add re-routing to https for production, session cookie middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
-const app = express();
 const compression = require("compression");
+const cookieSession = require("cookie-session");
 const path = require("path");
 const authRouter = require("./routers/auth-router");
+const app = express();
 const PORT = 3001;
 
 // MIDDLEWARE
+if (process.env.NODE_ENV == "production") {
+    app.use((req, res, next) => {
+        if (req.headers["x-forwarded-proto"].startsWith("https")) {
+            return next();
+        }
+        res.redirect(`https://${req.hostname}${req.url}`);
+    });
+}
+app.use(
+    cookieSession({
+        secret:
+            process.env.SESSION_SECRET ||
+            require("./secrets").COOKIE_SESSION_SECRET,
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+        sameSite: true,
+    })
+);
 app.use(compression());
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 app.use(express.json());
