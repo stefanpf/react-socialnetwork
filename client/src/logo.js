cbd720bb2e8dd1901b49ffc70caf8fe018458209
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-11 14:20:08 +0100

    add web fonts

--- client/src/logo.js
+++ client/src/logo.js
@@ -1,3 +1,8 @@
 export default function Logo() {
-    return <img src="/img/logo.png" className="logo-image" alt="logo" />;
+    return (
+        <>
+            <img src="/img/logo.png" className="logo-image" alt="logo" />
+            <a href="/logout">Logout</a>
+        </>
+    );
 }
