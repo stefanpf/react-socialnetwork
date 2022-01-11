3d60127cd74c22a5c702fc68ee9d577fd90048c3
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-11 14:19:41 +0100

    add basic SES setup

---
+++ server/utils/ses.js
@@ -0,0 +1,37 @@
+const aws = require("aws-sdk");
+
+let secrets;
+if (process.env.NODE_ENV == "production") {
+    secrets = process.env;
+} else {
+    secrets = require("../../secrets");
+}
+
+const ses = new aws.SES({
+    accessKeyId: secrets.AWS_KEY,
+    secretAccessKey: secrets.AWS_SECRET,
+    region: "eu-central-1",
+});
+
+module.exports.sendEmail = function (recipient, subject, body) {
+    return ses
+        .sendEmail({
+            Source: "SES Test <stefanmpfeuffer@gmail.com>",
+            Destination: {
+                ToAddresses: [recipient],
+            },
+            Message: {
+                Body: {
+                    Text: {
+                        Data: body,
+                    },
+                },
+                Subject: {
+                    Data: subject,
+                },
+            },
+        })
+        .promise()
+        .then(() => console.log("it worked!"))
+        .catch((err) => console.log(err));
+};
