52b2407511421aad99c9547f0c5507ad067aa318
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-18 12:51:25 +0100

    don't need to pas these props to the BioEditor

--- client/src/profile-components/profile.js
+++ client/src/profile-components/profile.js
@@ -12,8 +12,6 @@ export default function Profile(props) {
             />
             <BioEditor
                 userId={props.userId}
-                first={props.first}
-                last={props.last}
                 bio={props.bio}
                 updateBioFunc={props.updateBioFunc}
             />
