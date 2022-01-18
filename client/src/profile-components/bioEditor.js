59f051658df4390af36298a32a85a46437afa40f
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-18 10:27:44 +0100

    remove redundant logic

--- client/src/profile-components/bioEditor.js
+++ client/src/profile-components/bioEditor.js
@@ -23,7 +23,10 @@ export default class BioEditor extends Component {
     }
 
     updateBio() {
-        if (this.state.bioDraft || this.state.bioDraft == "") {
+        if (
+            this.state.bioDraft != this.props.bio ||
+            this.state.bioDraft == ""
+        ) {
             fetch(`/api/user/profile/${this.props.userId}`, {
                 method: "POST",
                 headers: {
